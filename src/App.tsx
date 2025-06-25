2001e8a65940d5c039da5a59320f2f3747803128
tkdgh7063 <hoo412298@gmail.com>
2025-06-25 16:44:03 +0900

    Add board creation with validation
    - Implemented form for adding new boards using useForm
    - Prevented duplicate board names and displayed error message
    - Automatically removed error message after 5 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { flushSync } from "react-dom";
+import { useForm } from "react-hook-form";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { ToDoStateProps, toDoState } from "./atoms";
@@ -7,6 +9,7 @@ import Board from "./Components/Board";
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   height: 100vh;
   width: 100vw;
   margin: 0 auto;
@@ -21,6 +24,44 @@ const Boards = styled.div`
   width: 100%;
   gap: 15px;
 `;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BoardInput = styled.input<InputProps>`
+  width: 25%;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  text-align: center;
+  margin-bottom: ${({ hasError }) => (hasError ? "5px" : "30px")};
+  &:focus {
+    outline: none;
+    border: 2px solid #74b9ff;
+    padding-top: 8px;
+    padding-bottom: 6px;
+    margin-bottom: ${({ hasError }) => (hasError ? "7px" : "32px")};
+  }
+`;
+
+const ErrorText = styled.p`
+  color: red;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+interface FormProps {
+  board: string;
+}
+
+interface InputProps {
+  hasError?: boolean;
+}
 
 function App() {
   const onDragEnd = ({ destination, source }: DropResult) => {
@@ -64,9 +105,47 @@ function App() {
   };
 
   const [category, setToDos] = useRecoilState(toDoState);
+  const [error, setError] = useState("");
+  const { register, setValue, handleSubmit } = useForm<FormProps>();
+  const onValid = ({ board }: FormProps) => {
+    setToDos((allBoards) => {
+      if (allBoards.hasOwnProperty(board)) {
+        setError("Board already exists");
+        return allBoards;
+      }
+
+      setError("");
+      const newBoard = { id: Date.now(), text: board };
+      return {
+        ...allBoards,
+        [newBoard.text]: [],
+      };
+    });
+    setValue("board", "");
+  };
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError("");
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
+        <Form onSubmit={handleSubmit(onValid)}>
+          <BoardInput
+            type="text"
+            placeholder="Add new board"
+            {...register("board", { required: true })}
+            hasError={Boolean(error)}
+          />
+          {error && <ErrorText>{error}</ErrorText>}
+        </Form>
         <Boards>
           {Object.keys(category).map((boardId) => (
             <Board key={boardId} boardId={boardId} toDos={category[boardId]} />
